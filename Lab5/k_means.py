9dd5482d640c0eee002b4c64b3262daa2bdbb472
Midax2 <dmytro.dzhusov@gmail.com>
2024-04-27 14:27:54 +0200

    Uploading updated version of Laboratory 5

--- Lab5/k_means.py
+++ Lab5/k_means.py
@@ -1,41 +1,61 @@
 import numpy as np
 
+
 def initialize_centroids_forgy(data, k):
     # TODO implement random initialization
-    return None
+    centroids_idx = np.random.choice(data.shape[0], k, replace=False)
+    centroids = data[centroids_idx]
+    return centroids
+
 
 def initialize_centroids_kmeans_pp(data, k):
     # TODO implement kmeans++ initizalization
-    return None
+    centroids = []
+    centroids.append(data[np.random.randint(data.shape[0])])
+    for _ in range(1, k):
+        distances = np.array([min([np.linalg.norm(x - c) for c in centroids]) for x in data])
+        probabilities = distances / distances.sum()
+        cumulative_probabilities = probabilities.cumsum()
+        r = np.random.rand()
+        for j, p in enumerate(cumulative_probabilities):
+            if r < p:
+                i = j
+                break
+        centroids.append(data[i])
+    return np.array(centroids)
+
 
 def assign_to_cluster(data, centroid):
     # TODO find the closest cluster for each data point
-    return None
+    distances = np.linalg.norm(data[:, np.newaxis, :] - centroid, axis=2)
+    return np.argmin(distances, axis=1)
+
 
 def update_centroids(data, assignments):
     # TODO find new centroids based on the assignments
-    return None
+    centroids = np.array([data[assignments == i].mean(axis=0) for i in range(len(np.unique(assignments)))])
+    return centroids
+
 
 def mean_intra_distance(data, assignments, centroids):
-    return np.sqrt(np.sum((data - centroids[assignments, :])**2))
+    return np.sqrt(np.sum((data - centroids[assignments, :]) ** 2))
 
-def k_means(data, num_centroids, kmeansplusplus= False):
+
+def k_means(data, num_centroids, kmeansplusplus=False):
     # centroids initizalization
     if kmeansplusplus:
         centroids = initialize_centroids_kmeans_pp(data, num_centroids)
-    else: 
+    else:
         centroids = initialize_centroids_forgy(data, num_centroids)
 
-    
-    assignments  = assign_to_cluster(data, centroids)
-    for i in range(100): # max number of iteration = 100
-        print(f"Intra distance after {i} iterations: {mean_intra_distance(data, assignments, centroids)}")
+    assignments = assign_to_cluster(data, centroids)
+    for i in range(100):  # max number of iteration = 100
+        # print(f"Intra distance after {i} iterations: {mean_intra_distance(data, assignments, centroids)}")
         centroids = update_centroids(data, assignments)
         new_assignments = assign_to_cluster(data, centroids)
-        if np.all(new_assignments == assignments): # stop if nothing changed
+        if np.all(new_assignments == assignments):  # stop if nothing changed
             break
         else:
             assignments = new_assignments
 
-    return new_assignments, centroids, mean_intra_distance(data, new_assignments, centroids)         
-
+    return new_assignments, centroids, mean_intra_distance(data, new_assignments, centroids)
--- Lab5/main.py
+++ Lab5/main.py
@@ -2,20 +2,22 @@ from k_means import k_means
 import pandas as pd
 import numpy as np
 
+
 def load_iris():
     data = pd.read_csv("data/iris.data", names=["sepal_length", "sepal_width", "petal_length", "petal_width", "class"])
-    print(data)
+    # print(data)
     classes = data["class"].to_numpy()
     features = data.drop("class", axis=1).to_numpy()
     return features, classes
 
+
 def evaluate(clusters, labels):
     for cluster in np.unique(clusters):
-        labels_in_cluster = labels[clusters==cluster]
-        print(f"Cluster: {cluster}")
-        for label_type in np.unique(labels):
-            print(f"Num of {label_type}: {np.sum(labels_in_cluster==label_type)}")
-    
+        labels_in_cluster = labels[clusters == cluster]
+        # print(f"Cluster: {cluster}")
+        # for label_type in np.unique(labels):
+            # print(f"Num of {label_type}: {np.sum(labels_in_cluster == label_type)}")
+
 
 def clustering(kmeans_pp):
     data = load_iris()
@@ -27,6 +29,7 @@ def clustering(kmeans_pp):
         intra_class_variance.append(error)
     print(f"Mean intra-class variance: {np.mean(intra_class_variance)}")
 
-if __name__=="__main__":
-    clustering(kmeans_pp = True)
-    # clustering(kmeans_pp = False)
+
+if __name__ == "__main__":
+    clustering(kmeans_pp=True)
+    clustering(kmeans_pp=False)
